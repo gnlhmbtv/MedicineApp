3172c16832d686ac25124b08d1b3c8549b43c98d
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2021-10-21 08:56:23 +0400

    Medicine

---
+++ Utilies/Helper/Helper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utilies.Helper
+{
+    public static class Helper
+    {
+        public static void ChangeTextColor(ConsoleColor color, string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
