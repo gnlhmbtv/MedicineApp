2302cf9b3d95f244fbf7464ab1c963353580baf5
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2021-10-21 08:54:27 +0400

    Entities

---
+++ Entities/Models/Drug.cs
@@ -0,0 +1,12 @@
+﻿using Entities.Interfaces;
+
+
+namespace Entities.Models
+{
+    public class Drug : IEntity
+    {
+        public int Id { get ; set ; }
+        public string DrugName { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ Entities/Models/DrugType.cs
@@ -0,0 +1,12 @@
+﻿using Entities.Interfaces;
+
+
+namespace Entities.Models
+{
+    public class DrugType : IEntity
+    {
+        public int Id { get ; set ; }
+        public string TypeName { get; set; }
+        public int DrugCount { get; set; }
+    }
+}
